cf31c29c04176cd273ecec2c98825684c31a4575
GH-LIP <1006532092@qq.com>
2016-05-29 13:10:21 +0800

    I'm a green hand!

--- Hello.java
+++ Hello.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class Hello{
 	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		System.out.println("»¶Ó­À´µ½É¨À×ÓÎÏ·");
+		boolean gameflag = true;
+		while(gameflag){
+			
+
+		}
 
 	}
 
